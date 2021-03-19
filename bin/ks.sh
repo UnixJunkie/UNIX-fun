7d7ab11a6c2bb636839bbcce2a36167bcda453eb
Francois Berenger <unixjunkie@sdf.org>
2021-03-19 11:39:29 +0900

    new file:   bin/ks.sh

---
+++ bin/ks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#set -x # DEBUG
+
+# Kolmogorov-Smirnov test
+
+# usage:
+# 0     1  2    3  4
+# ks.sh f1 col1 f2 col2
+
+(R -q --no-save << EOF
+a <- scan(pipe("cut -d' ' -f$2 $1"), what="", sep="\n")
+b <- scan(pipe("cut -d' ' -f$4 $3"), what="", sep="\n")
+x <- as.numeric(a)
+y <- as.numeric(b)
+ks.test(x, y)
+EOF
+) #2>&1 | awk '/^D /{if($6 >= 0.05){print "KO"}}'
