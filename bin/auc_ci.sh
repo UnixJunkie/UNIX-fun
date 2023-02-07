96592214f67b9745938203fdcbc0c5b9f1b59727
Francois Berenger <unixjunkie@sdf.org>
2023-02-07 11:24:51 +0900

    auc_ci.sh: debugged ugly R script

--- bin/auc_ci.sh
+++ bin/auc_ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#set -x
+# given a score-labels file, compute its AUROC and 95% confidence interval
 
-# given a score-labels file, compute its AUC value and 95% confidence interval
+#set -x
+set -u
 
 if [ "$#" -ne "1" ] ; then
     #             0   1
@@ -15,25 +16,28 @@ INPUT=$1
 TMPDIR=`mktemp -d`
 trap "rm -rf $TMPDIR" EXIT
 
-F1=$TMPDIR"/scores"
-F2=$TMPDIR"/labels"
+SCORES=$TMPDIR"/scores"
+LABELS=$TMPDIR"/labels"
 
-awk '{print $1}' $INPUT > $F1
-awk '{print $2}' $INPUT > $F2
+awk '{print $1}' $INPUT > $SCORES
+awk '{print $2}' $INPUT > $LABELS
 
 Rscript <(
 cat <<EOF
 #!/usr/bin/Rscript
 
-library(pROC)
+library(pROC, quietly=TRUE)
 
 args <- commandArgs(TRUE)
 
-# vector of ratings for both raters
+# create numerical vectors
 scores <- read.table(args[1])
 labels <- read.table(args[2])
-roc_curve <- roc(labels, scores)
+# the roc function requires a data frame
+df <- data.frame(scores, labels)
+colnames(df) <- c("scores", "labels")
+roc_curve <- roc(df, "labels", "scores")
 auc(roc_curve)
 ci.auc(roc_curve)
 EOF
-) $F1 $F2
+) $SCORES $LABELS
