e46e0a4e2505cb2063d82d627a1d4a3be8fff30f
Francois Berenger <unixjunkie@sdf.org>
2023-11-01 11:31:50 +0900

    bin/mol_names.sh: support .xyz format

--- bin/mol_names.sh
+++ bin/mol_names.sh
@@ -8,6 +8,8 @@ for f in "$@"; do
     case "$extension" in
         mol2) grep -F -w -A1 '@<TRIPOS>MOLECULE' $f | grep -v '@<TRIPOS>MOLECULE' | grep -v '\-\-'
               ;;
+        xyz) grep '\t' $f | cut -f2
+              ;;
         *) echo ${0}" unsupported file format: ."$f
            ;;
     esac
